73642e497bbffd932e5257085f0fcbc5d2f20e19
kdy5487 <kdykdy5487@naver.com>
2025-04-21 02:49:33 +0900

    UI구성

---
+++ android/app/src/main/kotlin/com/example/fluuter_local_search_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fluuter_local_search_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fluuter_local_search_app/ui/detail/detail_page.dart';
+import 'package:fluuter_local_search_app/ui/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: DetailPage());
+  }
+}
